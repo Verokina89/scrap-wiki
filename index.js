0709d83d952510a09c1bed9c357ab98abcaf5d21
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-09-05 18:07:51 +0200

    acceso a pagina principal; enlaces e imagenes

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-const axios = require('axios')
-const cheerio = require('cheerio')
-const http = require('http')
-const express = require('express')
+const express = require('express') //crear servidor
+const axios = require('axios') //Hacer peticiones HTTP
+const cheerio = require('cheerio') //Para parsear el HTTP
+const http = require('http') //crear servido
+const app = express()
 
 const url = 'https://es.wikipedia.org/wiki/Categor%C3%ADa:M%C3%BAsicos_de_rap'
 
+app.get('/', (req, res) => {
+    axios.get(url).then((response) => {
+        const html = response.data;
+        const $ = cheerio.load(html)
+
+        const titleH1 = $('h1').text()
+
+        //arr para guardar enlaces y las img
+        const links = []
+        const imgs = []
+
+        //obtener enlaces desde #mw-pages
+        $('#mw-pages a').each((index, element) => {
+            const link = $(element).attr('href'); // Obtener atributo href de cada enlace
+            links.push(link);
+        });
+
+        // Obtener las imgs de la url principal.
+        $('img').each((index, element) => {
+            const img = $(element).attr('src'); //Obtiene atributo src de cada imagen
+            imgs.push(img);
+        });
+
+        
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`El Servidor está escuchando en el puerto http://localhost:${PORT}`);
+})
